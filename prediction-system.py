90d2b74ac3ad2a39fab259ccac893820a2c5fb10
tvkqd <tvkqd@mail.umkc.edu>
2019-11-10 13:52:24 -0800

    Update prediction-system.py

--- prediction-system.py
+++ prediction-system.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima_model import ARIMA
 import itertools
+import pmdarima as pm
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -72,21 +73,21 @@ if __name__ == "__main__":
                 data.at[i, 'Count'] = hour.get_value(j, 'Count')
                 break
 
-    stationary_check(data)
+    #stationary_check(data)
     #From the graph, we cannot apply stationary model for this time dataset
     #Scale data
     data_scaled = np.log(data)
 
     #Splitting train, test
     x = data_scaled['Count']
-    x_train = x[:475]
-    x_test = x[476:]
+    x_train = x[117:]
+    x_test = x[:116]
     indexs = x_test.index
     str_indexs = [i.strftime('%y-%m-%d %H:%M:%S') for i in indexs]
 
-    #Create model
-    ######################################
-    #Finding pqd
+    # #Create model
+    # ######################################
+    # #Finding pqd
     # p = d = q = range(0,5)
     # pdq = list(itertools.product(p,d,q))
     #
@@ -102,29 +103,88 @@ if __name__ == "__main__":
     #     except:
     #         continue
     # print(index)
-    ########################################
-    #Create model
+    # ########################################
+
     model = ARIMA(x_train, order=(6,0,2))
     arima = model.fit()
+    print(arima.summary())
     plt.plot(x_train)
     plt.plot(arima.fittedvalues, color='red')
     plt.show()
 
-    #Prediction
-    pred = []
-    for i in range(len(x_test)):
-        #THIS IS AN INTERVAL, NEED TO DO BE PLOTTED
-        output = arima.forecast()
-        print(output)
-        yhat = output[0]
-        pred.append(yhat)
-    error = mean_squared_error(x_test, pred)
-    print('Test MSE: %.3f' % error)
+    #Plotting the precdict and actual for test data
+    fc, se, conf = arima.forecast(116, alpha=0.1)  # 90% conf
+    fc_series = pd.Series(fc, index=x_test.index)
+    lower_series = pd.Series(conf[:, 0], index=x_test.index)
+    upper_series = pd.Series(conf[:, 1], index=x_test.index)
+    # Plot
+    plt.figure(1)
+    plt.figure(figsize=(12, 5), dpi=100)
+    plt.plot(x_train, label='training')
+    plt.plot(x_test, label='actual')
+    plt.plot(fc_series, label='forecast')
+    plt.fill_between(lower_series.index, lower_series, upper_series,
+                     color='k', alpha=.15)
+    plt.title('Forecast vs Actuals')
+    plt.legend(loc='upper left', fontsize=8)
+    plt.show(block= False)
 
-    pred = pd.DataFrame(pred)
-    pred['CreationTime'] = str_indexs
-    pred['CreationTime'] = pd.to_datetime(pred['CreationTime'])
-    pred = pred.set_index(pred.CreationTime)
-    pred = pred.drop(['CreationTime'], axis=1)
-    # NEED TO FIX THE PREDICTION
+    #Plotting future prediction
+    arima.plot_predict(400,592)
+    plt.figure(2)
+    plt.show(block= False)
+
+    #Drawing the future
+    smodel = pm.auto_arima(data, start_p=1, start_q=1,
+                           test='adf',
+                           max_p=6, max_q=2, m=30,
+                           start_P=0, seasonal=True,
+                           d=None, D=1, trace=True,
+                           error_action='ignore',
+                           suppress_warnings=True,
+                           stepwise=True)
+    print(smodel.summary())
+
+    # Forecast
+    n_periods = 30
+    fitted, confint = smodel.predict(n_periods=n_periods, return_conf_int=True)
+    index_of_fc = pd.date_range(data.index[-1], periods=n_periods, freq='MS')
+
+    # make series for plotting purpose
+    fitted_series = pd.Series(fitted, index=index_of_fc)
+    lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+    upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+
+    # Plot
+    plt.figure(3)
+    plt.plot(data)
+    plt.plot(fitted_series, color='darkgreen')
+    plt.fill_between(lower_series.index,
+                     lower_series,
+                     upper_series,
+                     color='k', alpha=.15)
+    plt.show()
+
+    #Reference: https://www.machinelearningplus.com/time-series/arima-model-time-series-forecasting-python/
+
+    # #Prediction
+    # output = arima.forecast(steps=10)[0]
+    # print(output)
+    #
+    # pred = []
+    # for i in range(len(x_test)):
+    #     #THIS IS AN INTERVAL, NEED TO DO BE PLOTTED
+    #     output = arima.forecast(steps=10)
+    #     print(output)
+    #     yhat = output[0]
+    #     pred.append(yhat)
+    # error = mean_squared_error(x_test, output)
+    # print('Test MSE: %.3f' % error)
+
+    # pred = pd.DataFrame(pred)
+    # pred['CreationTime'] = str_indexs
+    # pred['CreationTime'] = pd.to_datetime(pred['CreationTime'])
+    # pred = pred.set_index(pred.CreationTime)
+    # pred = pred.drop(['CreationTime'], axis=1)
+    # # NEED TO FIX THE PREDICTION
 
